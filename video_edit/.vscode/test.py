af6974be58cbb26dd160e6faa5ff21ec812bd5fa
maxmatu <maxmatu@windowslive.com>
2020-03-26 16:48:30 +0000

    ldr sorted

--- video_edit/.vscode/test.py
+++ video_edit/.vscode/test.py
@@ -2,6 +2,8 @@ import numpy as np
 import pandas as pd
 from nptdms import TdmsFile
 from matplotlib import pyplot as plt
+import sys
+sys.path.append("C:\\Users\\maxma\\Dropbox (UCL - SWC)\\Project_spiders\\Analysis\\spider_behav_analysis\\video_edit")
 from LDR import get_stimuli 
 
 tdms_path = 'C:\\Users\\maxma\\Dropbox (UCL - SWC)\\Project_spiders\\Analysis\\confined_shade_arena_escape\\06_03_20_sp8\\spider_camera_ldr(0).tdms' 
@@ -10,4 +12,3 @@ photodiode_save_path = r'C:\Users\maxma\Dropbox (UCL - SWC)\Project_spiders\Anal
 fps = 40
 sampling_rate = 25000
 stimuli = get_stimuli(tdms_path, photodiode_save_path, fps, sampling_rate, 8)
-print(stimuli)
